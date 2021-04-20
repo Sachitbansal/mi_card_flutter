a080136800c0004d5297f69061981d0f7c092aa2
Sachitbansal <44609504+Sachitbansal@users.noreply.github.com>
2021-04-21 01:47:39 +0530

    mi card flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp()
+  );
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.white,
+                child: Text('Container 1'),
+              ),
+              Container(width: 100.0,
+              height: 100.0,
+              color: Colors.blue,
+              child: Text('Container 2'),
+              ),
+              Container(width: 100.0,
+                height: 100.0,
+                color: Colors.red,
+                child: Text('Container 3'),
+              ),
+              Container
+                (width: 100.0,
+                height: 100.0,
+                color: Colors.yellow,
+                child: Text('Container 4'),
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
